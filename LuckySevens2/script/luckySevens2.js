d55f54f60da6100c0a2b4bd183d5a43689d2306f
dmichalb <58150349+dmichalb@users.noreply.github.com>
2019-12-07 15:09:30 -0600

    Update luckySevens2.js

--- LuckySevens2/script/luckySevens2.js
+++ LuckySevens2/script/luckySevens2.js
@@ -1,49 +1,38 @@
-function validateForm (){
-    var bet = document.forms["luckySevens"]["bet"].value;
-    if (bet <= 0){
-    document.getElementById("error-bet").innerHTML = "please make a bet larger than $0.00 *"
-    }
-}
 function rollDice() {
-    return Math.floor(math.random() * 6) + 1;
+    return Math.floor(Math.random() * 6) + 1;
 }
 function play() {
-    var bet = document.forms["luckySevens"]["bet"].value;
+    var bet = document.getElementById("bet").value;
     var money = bet;
-    var roll1 = rollDice();
-    var roll2 = rollDice();
-    var diceTotal = roll1+roll2;
-    var moneyArray = [];
+    var totalRolls = 0;
+    var maxMoney = bet;
+    var maxMoneyRoll = 0;
+    
+    if (bet <= 0){
+        alert("Please make a bet larger than $0.00 ");
+        return;
+    }
     
     while (money > 0) {
-    var roll1 = rollDice();
-    var roll2 = rollDice();
-    var diceTotal = roll1+roll2;
+        var roll1 = rollDice();
+        var roll2 = rollDice();
+        var diceTotal = roll1+roll2;
+        totalRolls++
         if (diceTotal != 7) {
-            money --;
+            money--;
         } else {
             money += 4
         }
-        moneyArray.push(money)
+        if (money > maxMoney) {
+            maxMoney = money;
+            maxMoneyRoll = totalRolls;
+        }
     }
-    var rollCounter = moneyArray.length;
-    var maxMoney = Math.max.apply(Math, moneyArray);
-    var highestPosition = moneyArray.indexOf(maxMoney);
-    var maxMoneyRoll = rollCounter - highestPosition;
     
-    function showResults() {
-        document.getElementById("resultsTable").style.display = "inline";
-        document.getElementById("playButton").innerHTML = "Play Again";
-        document.getElementById("resultBet").innerHTML = "$" + bet + ".00";
-        document.getElementById("resultRollCounter").innerHTML = rollCounter;
-        document.getElementById("resultMaxMoney").innerHTML = "$" + maxMoney + ".00";
-        document.getElementById("resultMaxMoneyRoll").innerHTML = maxMoneyRoll;
-    };
-    showResults();
-    }
-function unhide(resultsTable) {
-    var item = document.getElementById("resultsTable");
-    if (item) {
-        item.className=(item.className=="hidden")?"unhidden":"hidden";
-    }
+    document.getElementById("resultsTable").style.display = "inline";
+    document.getElementById("playButton").value = "Play Again";
+    document.getElementById("resultBet").innerHTML = "$" + bet + ".00";
+    document.getElementById("resultRollCounter").innerHTML = totalRolls;
+    document.getElementById("resultMaxMoney").innerHTML = "$" + maxMoney + ".00";
+    document.getElementById("resultMaxMoneyRoll").innerHTML = maxMoneyRoll;
 }
